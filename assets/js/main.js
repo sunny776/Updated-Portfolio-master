b633a3f449a10c53ab55e944984be47c23bfed4d
Sarness Prasad <sunny776@yahoo.com>
2021-03-10 13:54:22 -0800

    uploading my files

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//   });
+
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
